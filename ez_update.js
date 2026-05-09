96f2efd444d17db60b231e67ee33277113183c75
Hssmnoy <110244508+Hssmnoy@users.noreply.github.com>
2026-05-09 09:20:46 +0700

    Update ez_update.js

--- ez_update.js
+++ ez_update.js
@@ -19,7 +19,7 @@ const CATEGORIES = [
   "/movies/หนังผจญภัย-adventure",
   "/movies/หนังเกาหลี",
   "/movies/หนังญี่ปุ่น",
-  "/movies/หนังผู้ใหญ่-18-adult",
+  
 ];
 
 const TEST_MODE = false;   //false//true;
